2f3d03e9054e4e9f3d0f22160b9b8463cfb6fd68
BHAVIK <bhavikprajapati4244@gmail.com>
2023-06-03 09:19:34 +0530

    done app

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+// import dotenv from "dotenv";
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    // "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Key": "b17f8d13d7msh5af25edc69348c6p1df049jsn6e4052cbc1fe",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  // console.log(data);
+
+  return data;
+};
